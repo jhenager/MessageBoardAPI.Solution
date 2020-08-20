284e6530e6ed2c8aad581598cfcc0e46697a377a
James Henager <jameschenager@gmail.com>
2020-08-20 11:06:04 -0700

    add async methods to post and thread models
    Co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- MessageBoardClient/Models/Post.cs
+++ MessageBoardClient/Models/Post.cs
@@ -13,5 +13,44 @@ namespace MessageBoardClient.Models
     public DateTime CreationDate { get; set; }
     public int ParentThreadId { get; set; }
     public Thread ParentThread { get; set; }
+
+    public static List<Post> GetAll()
+    {
+      ApiHelper apicallTask = ApiHelper.GetAll("posts");
+      var result = apicallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Post> postList = JsonConvert.DeserializeObject<Post>(jsonResponse.ToString());
+
+      return postList;
+    }
+
+    public static Post GetDetails(int id)
+    {
+      ApiHelper apiCallTask = ApiHelper.Get("posts", id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Post post = JsonConvert.DeserializeObject<Post>(jsonResponse.ToString());
+
+      return post;
+    }
+
+    public static void Post(Post post)
+    {
+      string jsonPost = JsonConvert.SerializeObject(post);
+      var apiCallTask = ApiHelper.Post("posts", jsonPost);
+    }
+
+    public static void Put(Post post)
+    {
+      string jsonPost = JsonConvert.SerializeObject(post);
+      var apiCallTask = ApiHelper.Put("posts", post.PostId, jsonPost);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete("posts", id);
+    }
   }
 }
--- MessageBoardClient/Models/Thread.cs
+++ MessageBoardClient/Models/Thread.cs
@@ -18,5 +18,44 @@ namespace MessageBoardClient.Models
     {
       this.Posts = new HashSet<Post>();
     }
+
+    public static List<Thread> GetAll()
+    {
+      ApiHelper apicallTask = ApiHelper.GetAll("threads");
+      var result = apicallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Thread> threadList = JsonConvert.DeserializeObject<Thread>(jsonResponse.ToString());
+
+      return threadList;
+    }
+
+    public static Post GetDetails(int id)
+    {
+      ApiHelper apiCallTask = ApiHelper.Get("posts", id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Thread thread = JsonConvert.DeserializeObject<Thread>(jsonResponse.ToString());
+
+      return thread;
+    }
+
+    public static void Post(Thread thread)
+    {
+      string jsonBoard = JsonConvert.SerializeObject(thread);
+      var apiCallTask = ApiHelper.Post("threads", jsonBoard);
+    }
+
+    public static void Put(Thread thread)
+    {
+      string jsonThread = JsonConvert.SerializeObject(thread);
+      var apiCallTask = ApiHelper.Put("threads", thread.ThreadId, jsonThread);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete("threads", id);
+    }
   }
 }

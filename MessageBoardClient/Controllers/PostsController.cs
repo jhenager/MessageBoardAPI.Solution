ed5806b09ce396f1d8595a118b40969aa3188bf3
James Henager <jameschenager@gmail.com>
2020-08-20 11:30:38 -0700

    add routes to postscontroller
    Co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- MessageBoardClient/Controllers/PostsController.cs
+++ MessageBoardClient/Controllers/PostsController.cs
@@ -5,3 +5,59 @@ using MessageBoardClient.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq;
 using System.Threading.Tasks;
+
+namespace MessageBoardClient.Controllers
+{
+  public class PostsController : Controller
+  {
+    public IActionResult Index()
+    {
+      List<Post> allPosts = Board.GetAll();
+      return View(allPosts);
+    }
+
+    public IActionResult Details(int id)
+    {
+      Post thisPost = Post.GetDetails(id);
+      return View(thisPost);
+    }
+
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Post post)
+    {
+      Post.Post(post);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Update(int id)
+    {
+      Post thisPost = Post.GetDetails(id);
+      return View(thisPost);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Post post)
+    {
+      Post.Put(post);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Post thisPost = Post.GetDetails(id);
+      return View(thisPost);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Post post)
+    {
+      Post.Delete(post.PostId);
+      return RedirectToAction("Index");
+    }
+  }
+}
